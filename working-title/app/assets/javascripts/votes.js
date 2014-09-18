28d8dda02dffc131d7cc5391730658afadaad656
Peggy Fox & Ryan Bahniuk <pair+peggyfox.ryanbahniuk@devbootcamp.com>
2014-09-18 17:08:57 -0500

    REMOVING TURBOLINKS!!!!!!!!!!!

--- working-title/app/assets/javascripts/votes.js
+++ working-title/app/assets/javascripts/votes.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $(".new_vote").on("submit", function(event){
     event.preventDefault();
-    data = $(this).serialize();
-    request = $.ajax("/votes", {"method": "post", data: data})
+    var data = $(this).serialize();
+    var request = $.ajax("/votes", {"method": "post", data: data})
     request.done(function(response) {
       $(".score").html(response)
     });
