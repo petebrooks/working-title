e0b47f2ac82af2cc4a8f4d1ef9f40518d81181b4
Ryan Bahniuk <ryan.bahniuk@gmail.com>
2014-09-18 22:49:02 -0500

    adding some version controller tests

--- spec/controllers/versions_controller_spec.rb
+++ spec/controllers/versions_controller_spec.rb
@@ -1,12 +1,11 @@
 require "rails_helper"
 
-# describe VersionsController do
-  # let(:project) { Version.create(name: "as;dlkfja;sldkfja")}
-  # let(:version) { Version.create(contribution: "testesteststestststsetset", contributor_id: 1, project_id: 1)}
-  # let(:version_2) { Version.create(contribution: "testesteststestststsetset", contributor_id: 1, previous_version: version)}
+describe VersionsController do
+	let(:project) { Version.create(name: "as;dlkfja;sldkfja")}
+	let(:version) { Version.create(contribution: "testesteststestststsetset", contributor_id: 1, project_id: 1)}
+	let(:version_2) { Version.create(contribution: "testesteststestststsetset", contributor_id: 1, previous_version: version)}
 
-#   it { should belong_to(:previous_version) }
-#   it { should belong_to(:contributor) }
-#   it { should validate_presence_of(:project) }
-# end
-
+	it { should belong_to(:previous_version) }
+	it { should belong_to(:contributor) }
+	it { should validate_presence_of(:project) }
+end
