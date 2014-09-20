6c2973759be43cba42fb4ec1cc065c25ffc88dd9
peggyfox <fox.peggy@gmail.com>
2014-09-20 18:14:15 -0500

    category page master resolutions

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
   //get controller to give json of projects
+  //take json objects - insert into dom
+  // when sort - take projects from all projects, sort, remove all projects and add all sorted projects
 
   $("#sort_field").on("click", function(event){
     event.preventDefault();
