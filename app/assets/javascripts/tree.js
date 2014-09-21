7d075ad215742b914b3de8128d73d523ba9cb8cc
Pete Brooks <peterossbrooks@gmail.com>
2014-09-21 14:14:19 -0500

    legit tree

--- app/assets/javascripts/tree.js
+++ app/assets/javascripts/tree.js
@@ -1,78 +1,93 @@
 $(document).ready(function(){
 
-var treeData = $('.tree_data').data('data');
-var height = 500;
-var width = 1000;
+  var treeData = $('.tree_data').data('data');
+  var height = 500;
+  var width = 1000;
 
-var tree = d3.layout.tree()
-  .sort(null)
-  .size([height, width])
-  .children(function(d) {
-    return (!d.children || d.children.length === 0) ? null : d.children;
-  });
+  var nodeColor = "blue";
+  var highlightColor = "red";
 
-var nodes = tree.nodes(treeData);
-var links = tree.links(nodes);
+  var tree = d3.layout.tree()
+    .sort(null)
+    .size([height, width])
+    .children(function(d) {
+      return (!d.children || d.children.length === 0) ? null : d.children;
+    });
 
-var svg = d3.select("body div.tree")
-  .append("svg")
-  .attr('width', 1435)
-  .attr('height', 600)
-  .append("g");
+  var diagonal = d3.svg.diagonal()
+    .projection(function(d) { return [d.y, d.x]; });
 
-var link = d3.svg.diagonal()
-  .projection(function(d){
-    return [d.y, d.x];
-  });
+  var svg = d3.select("body div.tree")
+    .append("svg")
+    .attr('width', width)
+    .attr('height', height)
+    .append("g")
+    .attr('width', width)
+    .attr('height', height);
 
-svg.selectAll("path.link")
-  .data(links)
-  .enter()
-  .append('path')
-  .attr('class', 'link')
-  .attr('d', link);
+  var i = 0;
+  root = treeData;
 
-var nodeGroup = svg.selectAll("g.node")
-  .data(nodes)
-  .enter()
-  .append('g')
-  .attr('class', 'node')
-  .attr('transform', function(d){
-    return "translate(" + d.y + "," + d.x + ")";
-  });
+  update(root);
 
-nodeGroup.append('circle')
-  .attr('class', 'node-dot')
-  .attr('r', 5);
+  function update(source) {
+
+    var nodes = tree.nodes(root);
+    var links = tree.links(nodes);
+
+    nodes.forEach(function(d){
+      d.y = d.depth * 40 + 10;
+    });
+
+    var node = svg.selectAll('g.node')
+      .data(nodes, function(d) {
+        return d.nodeid || (d.nodeid = i++);
+      });
+
+    var nodeEnter = node.enter().append('g')
+      .attr('class', 'node')
+      .attr('id', function(d) { return d.nodeid; })
+      .attr('transform', function(d) {
+        return 'translate(' + d.y + ',' + d.x + ')';
+      })
+      .on('mouseover', function(d) {
+        d3.select(this).insert("text")
+        .text(function(d) { return d.contribution})
+        .attr("flood-color", "white");
+      })
+      .on('mouseout', function(d) {
+        d3.select(this).select("text").remove();
+      });
+
+    nodeEnter.append('circle')
+      // .attr('cx', function(d){ return d.y; })
+      // .attr('cy', function(d){ return d.x; })
+      .attr("r", 10)
+      .style("fill", nodeColor);
+
+
+    // nodeEnter.append("text")
+    //   .attr("x", function(d) {
+    //     return d.children || d._children ? -13 : 13;
+    //   })
+    //   .attr("dy", "0.35em")
+    //   .attr("text-anchor", function(d) {
+    //     return d.children || d._children ? "end" : "start";
+    //   })
+    //   .text(function(d) { return d.contribution; })
+    //   .style("fill-opacity", 1);
+
+    var link = svg.selectAll("line.link")
+      .data(links, function(d) { return d.target.id });
+
+    link.enter().insert("line", "g")
+      .attr("class", "link")
+      // .attr("d", diagonal);
+      .attr("y1", function(d) { return d.source.x })
+      .attr("x1", function(d) { return d.source.y })
+      .attr("y2", function(d) { return d.target.x })
+      .attr("x2", function(d) { return d.target.y })
+      .attr("stroke", "black");
+  }
 
 });
-
-// var treeData2 = JSON.parse(gon.tree_data);
-
-// var treeData = [
-//   {
-//     "name": "Top Level",
-//     "parent": "null",
-//     "children": [
-//       {
-//         "name": "Level 2: A",
-//         "parent": "Top Level",
-//         "children": [
-//           {
-//             "name": "Son of A",
-//             "parent": "Level 2: A"
-//           },
-//           {
-//             "name": "Daughter of A",
-//             "parent": "Level 2: A"
-//           }
-//         ]
-//       },
-//       {
-//         "name": "Level 2: B",
-//         "parent": "Top Level"
-//       }
-//     ]
-//   }
-// ];
-
