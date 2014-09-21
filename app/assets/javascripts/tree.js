14396cde52499e67134055291580bb49e1d813e4
Pete Brooks <peterossbrooks@gmail.com>
2014-09-21 17:05:46 -0500

    links work

--- app/assets/javascripts/tree.js
+++ app/assets/javascripts/tree.js
@@ -7,12 +7,12 @@ $(document).ready(function(){
   var nodeColor = "blue";
   var highlightColor = "red";
 
-  var tree = d3.layout.tree()
-    .sort(null)
+  var tree = d3.layout.force()
     .size([height, width])
-    .children(function(d) {
-      return (!d.children || d.children.length === 0) ? null : d.children;
-    });
+    // .children(function(d) {
+    //   return (!d.children || d.children.length === 0) ? null : d.children;
+    // })
+    .on("tick", tick);
 
   var diagonal = d3.svg.diagonal()
     .projection(function(d) { return [d.y, d.x]; });
@@ -23,13 +23,8 @@ $(document).ready(function(){
     .attr('height', height)
     .append("g")
     .attr('width', width)
-    .attr('height', height)
-    .call(d3.behavior.zoom().scaleExtent([1, 8]).on("click", zoom));
-
-  // $(document).on("click", function() {
-  //   // svg.transition().attr("transform", "rotate(90deg)");
-  //   svg.transition().attr("opacity", 0);
-  // });
+    .attr('height', height);
+    // .call(d3.behavior.zoom().scaleExtent([1, 8]).on("click", zoom));
 
   function zoom() {
     svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
@@ -44,8 +39,16 @@ $(document).ready(function(){
   update();
 
   function update() {
-    var nodes = tree.nodes(root);
-    var links = tree.links(nodes);
+    var nodes = d3.layout.tree().nodes(root);
+    var links = d3.layout.tree().links(nodes);
+
+    tree
+      .nodes(nodes)
+      .links(links)
+      .start();
+
+    node = node.data(nodes);
+    link = link.data(links);
 
     var voteRange = d3.extent(nodes.map(function(d) {
       return d.voteScore;
@@ -55,43 +58,34 @@ $(document).ready(function(){
       .domain(voteRange)
       .range([2, 18]);
 
-    nodes.forEach(function(d){
-      // d.x = d.depth * 40 + 10;
-    });
-
-    var nodeEnter = node.data(nodes)
-      .enter().append('g')
+    node.enter()
+      .append('g')
       .attr('class', 'node')
-      .attr('id', function(d) { return d.nodeid; })
       .attr('transform', function(d) {
-        return 'translate(' + d.y + ',' + d.x + ')';
-      })
-      // .on('mouseover', function(d) {
-      //   d3.select(this).insert("text")
-      //   .text(function(d) { return d.contribution})
-      //   .attr("flood-color", "white");
-      // })
-      // .on('mouseout', function(d) {
-      //   d3.select(this).select("text").remove();
-      // });
-      .on("click", function(d) {console.log(d.id)});
-
-    nodeEnter.append('circle')
-      .attr("r", function(d) { return voteScale(d.voteScore)})
+        return 'translate(' + 50 + ',' + d.x + ');' })
+      .append('circle')
+      .attr("r", function(d) { return voteScale(d.voteScore) })
       .style("fill", nodeColor);
 
-    link.data(links)
-      .enter().insert("line", "g")
+    link.enter()
+      .insert("line", "g")
       .attr("class", "link")
       .attr("x1", function(d) { return d.source.y })
       .attr("y1", function(d) { return d.source.x })
       .attr("x2", function(d) { return d.target.y })
       .attr("y2", function(d) { return d.target.x })
       .attr("stroke", "black");
+      debugger;
   };
 
   function tick() {
-
+    link.attr("x1", function(d) { return d.source.y })
+        .attr("y1", function(d) { return d.source.x })
+        .attr("x2", function(d) { return d.target.y })
+        .attr("y2", function(d) { return d.target.x });
+    node.attr("transform", function(d) {
+      return "translate(" + 50, "," + d.x + ");"
+    });
   };
 
 });
