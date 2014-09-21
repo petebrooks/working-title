072b92e02da12e1653f7a4134e9a73917b192401
Ryan Bahniuk <ryan.bahniuk@gmail.com>
2014-09-20 21:16:09 -0500

    fixed bug with jquery ui shake

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,3 +1,31 @@
+if ($.ui) {
+    (function () {
+        var oldEffect = $.fn.effect;
+        $.fn.effect = function (effectName) {
+            if (effectName === "shake") {
+                var old = $.effects.createWrapper;
+                $.effects.createWrapper = function (element) {
+                    var result;
+                    var oldCSS = $.fn.css;
+
+                    $.fn.css = function (size) {
+                        var _element = this;
+                        var hasOwn = Object.prototype.hasOwnProperty;
+                        return _element === element && hasOwn.call(size, "width") && hasOwn.call(size, "height") && _element || oldCSS.apply(this, arguments);
+                    };
+
+                    result = old.apply(this, arguments);
+
+                    $.fn.css = oldCSS;
+                    return result;
+                };
+            }
+            return oldEffect.apply(this, arguments);
+        };
+    })();
+}
+
+
 $(document).ready(function(){
 	$('#login').on("submit", function(event){
 		event.preventDefault();
@@ -12,6 +40,7 @@ $(document).ready(function(){
 				window.location.replace(response)
 			} else {
 				form.parent().effect( "shake" );
+				form.find("#password").val("");
 			}
 		});
 	});
