5bed6bbd0a162066ded9084accd74292a2a2f364
peggyfox <fox.peggy@gmail.com>
2014-09-20 19:52:25 -0500

    its a good start...

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -3,8 +3,15 @@ $(document).ready(function(){
   //take json objects - insert into dom
   // when sort - take projects from all projects, sort, remove all projects and add all sorted projects
 
-  $("#sort_field").on("click", function(event){
+  $(".sort_field").on("click", function(event){
     event.preventDefault();
+    var sort_id = $(this).attr("id")
+    var allProjects = $(".project_box")
+    //for each project, make a project object
+
+
+    debugger;
+
 
     // var data = $(this).serialize();
     // var request = $.ajax("/votes", {"method": "post", data: data})
