0162f48d21a78dc70a099f0b93d38e24b2ead3b5
Pete Brooks <peterossbrooks@gmail.com>
2014-09-21 18:27:29 -0500

    something...

--- app/assets/javascripts/tree.js
+++ app/assets/javascripts/tree.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   var treeData = $('.tree_data').data('data');
-  var height = 500;
+  var height = 1000;
   var width = 1000;
 
   var nodeColor = "blue";
@@ -15,7 +15,7 @@ $(document).ready(function(){
     });
 
   var diagonal = d3.svg.diagonal()
-    .projection(function(d) { return [d.y, d.x]; });
+    .projection(function(d) { return [d.x, d.y]; });
 
   var svg = d3.select("body div.tree")
     .append("svg")
@@ -25,14 +25,42 @@ $(document).ready(function(){
     .attr('width', width)
     .attr('height', height);
 
-  var i = 0;
   root = treeData;
 
+  function collapse(d) {
+    if (d.children) {
+      d._children = d.children;
+      d._children.forEach(collapse);
+      d.children = null;
+    }
+  }
+
+    var nodes = tree.nodes(root);
+
+    var voteRange = d3.extent(nodes.map(function(d) {
+      return d.voteScore;
+    }));
+
+    var voteScale = d3.scale.linear()
+      .domain(voteRange)
+      .range([2, 18]);
+
+
+  // var zoom = d3.behavior.zoom()
+  //   // .x(function(d) {return d.x})
+  //   // .y(function(d) {return d.y})
+  //   .center(function(d) { return [d.y, d.x] })
+  //   .scaleExtent([1, 10])
+  //   .on("zoom", runZoom);
+
+  // function runZoom() {
+  //   svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+  // }
+  root.children.forEach(collapse);
   update(root);
 
   function update(source) {
-
-    var nodes = tree.nodes(root);
+     var nodes = tree.nodes(root);
     var links = tree.links(nodes);
 
     nodes.forEach(function(d){
@@ -40,15 +68,17 @@ $(document).ready(function(){
     });
 
     var node = svg.selectAll('g.node')
-      .data(nodes, function(d) {
-        return d.nodeid || (d.nodeid = i++);
-      });
+      .data(nodes);
 
-    var nodeEnter = node.enter().append('g')
+    var link = svg.selectAll("line.link")
+      .data(links, function(d) { return d.target.id });
+
+    var nodeEnter = node.enter()
+      .append('g')
       .attr('class', 'node')
       .attr('id', function(d) { return d.nodeid; })
       .attr('transform', function(d) {
-        return 'translate(' + d.y + ',' + d.x + ')';
+        return 'translate(' + d.x + ',' + d.y + ')';
       })
       .on('mouseover', function(d) {
         d3.select(this).insert("text")
@@ -62,8 +92,17 @@ $(document).ready(function(){
     nodeEnter.append('circle')
       // .attr('cx', function(d){ return d.y; })
       // .attr('cy', function(d){ return d.x; })
-      .attr("r", 10)
-      .style("fill", nodeColor);
+      .attr("r", function(d) { return voteScale(d.voteScore) })
+      .style("fill", nodeColor)
+      .on("click", click);
+
+    d3.select('.node circle').style("fill", "red");
+
+    node.exit()
+      .remove();
+
+    link.exit()
+      .remove();
 
 
     // nodeEnter.append("text")
@@ -77,17 +116,27 @@ $(document).ready(function(){
     //   .text(function(d) { return d.contribution; })
     //   .style("fill-opacity", 1);
 
-    var link = svg.selectAll("line.link")
-      .data(links, function(d) { return d.target.id });
 
     link.enter().insert("line", "g")
       .attr("class", "link")
       // .attr("d", diagonal);
-      .attr("y1", function(d) { return d.source.x })
-      .attr("x1", function(d) { return d.source.y })
-      .attr("y2", function(d) { return d.target.x })
-      .attr("x2", function(d) { return d.target.y })
+      .attr("x1", function(d) { return d.source.x })
+      .attr("y1", function(d) { return d.source.y })
+      .attr("x2", function(d) { return d.target.x })
+      .attr("y2", function(d) { return d.target.y })
       .attr("stroke", "black");
   }
+
+  function click(d) {
+    if (d.children) {
+      d._children = d.children;
+      d.children = null;
+    } else {
+      d.children = d._children
+      d._children = null;
+    }
+    console.log(d);
+    update(d);
+  };
 
 });
